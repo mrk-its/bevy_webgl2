25a9a5c559de7d33aedc274fd8da54183ec7d210
Mariusz Kryński <mrk@sed.pl>
2020-11-23 00:19:45 +0100

    bumb bevy rev, update examples

--- examples/ui/text.rs
+++ examples/ui/text.rs
@@ -1,15 +1,30 @@
-use bevy::prelude::*;
+use bevy::{
+    diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin},
+    prelude::*,
+};
 
 /// This example illustrates how to create text and update it in a system. It displays the current FPS in the upper left hand corner.
 fn main() {
     App::build()
         .add_plugins(bevy_webgl2::DefaultPlugins)
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_startup_system(setup)
+        .add_system(text_update_system)
         .run();
 }
 
 // A unit struct to help identify the FPS UI component, since there may be many Text components
 struct FpsText;
+
+fn text_update_system(diagnostics: Res<Diagnostics>, mut query: Query<&mut Text, With<FpsText>>) {
+    for mut text in query.iter_mut() {
+        if let Some(fps) = diagnostics.get(FrameTimeDiagnosticsPlugin::FPS) {
+            if let Some(average) = fps.average() {
+                text.value = format!("FPS: {:.2}", average);
+            }
+        }
+    }
+}
 
 fn setup(commands: &mut Commands, asset_server: Res<AssetServer>) {
     commands
@@ -22,7 +37,7 @@ fn setup(commands: &mut Commands, asset_server: Res<AssetServer>) {
                 ..Default::default()
             },
             text: Text {
-                value: "Hello!".to_string(),
+                value: "FPS:".to_string(),
                 font: asset_server.load("fonts/FiraSans-Bold.ttf"),
                 style: TextStyle {
                     font_size: 60.0,
